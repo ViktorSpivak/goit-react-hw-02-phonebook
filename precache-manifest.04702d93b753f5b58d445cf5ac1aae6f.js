f2627615a3974494f367f420aac25aa4142e8ab0
Viktor Spivak <spivakmailbox@gmail.com>
2020-03-07 16:41:54 +0200

    Updates

---
+++ precache-manifest.04702d93b753f5b58d445cf5ac1aae6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62ca4cdf1012277d1974a8f3b40d71c",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d26fb1c3871b0d747e63",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.0bf037c4.chunk.css"
+  },
+  {
+    "revision": "11ea522fb6e15228bfc0",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8aa8c50a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8aa8c50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26fb1c3871b0d747e63",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.4150bfc1.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);

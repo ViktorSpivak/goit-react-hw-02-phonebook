9ef1a6872cf19a691f4b9264d4c476d359de3b96
Viktor Spivak <spivakmailbox@gmail.com>
2020-03-09 23:05:08 +0200

    fifth commit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,8 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import style from "./contactform.module.css";
 import { v4 as uuidv4 } from "uuid";
+import { isValidPhoneNumber } from "react-phone-number-input";
 export class ContactForm extends Component {
   state = {
     name: "",
@@ -11,24 +12,31 @@ export class ContactForm extends Component {
   numberFormId = uuidv4();
   handleChange = e => {
     const { name, value } = e.target;
+
     this.setState({ [name]: value });
   };
+  clearSetState() {
+    this.setState({
+      name: "",
+      number: ""
+    });
+  }
   handleSubmit = e => {
     e.preventDefault();
     const { name, number } = this.state;
     if (this.props.onFindOverlap(name)) {
       alert(`${name} is already in contacts`);
-      this.setState({
-        name: ""
-      });
+      this.clearSetState();
+      return;
+    }
+    if (!isValidPhoneNumber(number)) {
+      alert("Wrong number, use format +12133734253 (11 numbers");
+      this.clearSetState();
       return;
     }
     const newContact = { id: uuidv4(), name, number };
     this.props.onCheckIn(newContact);
-    this.setState({
-      name: "",
-      number: ""
-    });
+    this.clearSetState();
   };
   render() {
     const { name, number } = this.state;
